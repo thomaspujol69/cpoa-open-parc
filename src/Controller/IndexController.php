fd5d4f4303025aaf2729860205203f659a364056
LucasDaneyrolle <lucas.ddu69@gmail.com>
2021-12-14 17:58:19 +0100

    test php symfony mdr

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,8 +11,10 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
+        $liste = array('pizzas','Ana','spaghettis carbo','pâtes au saumon','blanquette de veau','escalope de dinde à la crème et aux champignons','Thomas','raclette');
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'controller_name' => 'Banane',
+            'liste' => $liste[array_Rand($liste)],
         ]);
     }
 }
