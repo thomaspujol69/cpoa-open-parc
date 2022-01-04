7683427711c45275e91c9a77c6f790ec524f1f16
LucasDaneyrolle <lucas.ddu69@gmail.com>
2022-01-04 15:50:37 +0100

    début tarids

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -22,7 +22,15 @@ class IndexController extends AbstractController
     public function tarifs(): Response
     {
         $r = $this->render('index/tarifs.html.twig', [
-            
+            'tarifs'=> [
+                'jour1' => ['jour' => 'Dimanche', 'cat1' => '30,00€', 'cat2' => '25,00€'],
+                'jour2' => ['jour' => 'Lundi', 'cat1' => '30,00€', 'cat2' => '25,00€'],
+                'jour3' => ['jour' => 'Mardi', 'cat1' => '30,00€', 'cat2' => '25,00€'],
+                'jour4' => ['jour' => 'Mercredi', 'cat1' => '40,00€', 'cat2' => '30,00€'],
+                'jour5' => ['jour' => 'Jeudi', 'cat1' => '45,00€', 'cat2' => '35,00€'],
+                'jour6' => ['jour' => 'Vendredi', 'cat1' => '60,00€', 'cat2' => '48,00€'],
+                'jour7' => ['jour' => 'Samedi', 'cat1' => '60,00€', 'cat2' => '45,00€'],
+            ]
         ]);
 
         return $r;
