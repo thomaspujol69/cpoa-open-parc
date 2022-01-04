fe3c0c96b5e2e352886dde4d9366867633af569f
LucasDaneyrolle <lucas.ddu69@gmail.com>
2022-01-04 11:17:04 +0100

    add tarifs

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,4 +17,14 @@ class IndexController extends AbstractController
 
         return $r;
     }
+
+    #[Route('/tarifs', name: 'tarifs')]
+    public function tarifs(): Response
+    {
+        $r = $this->render('index/index.html.twig', [
+            
+        ]);
+
+        return $r;
+    }
 }
