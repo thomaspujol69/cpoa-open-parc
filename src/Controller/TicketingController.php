663919d4405ccd3280e3a783f9e724227c8a1b04
Thomas PUJOL <tpujol00@gmail.com>
2022-01-10 23:29:04 +0100

    add redirect

--- src/Controller/TicketingController.php
+++ src/Controller/TicketingController.php
@@ -4,17 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class TicketingController extends AbstractController
 {
+    use TargetPathTrait;
+    
     #[Route('/billetterie', name: 'ticketing')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = $this->getUser();
         //print_r($user);
         if (!$user){
-            return $this->redirectToRoute('app_login', array('_target_path' => 'ticketing'));
+            $this->saveTargetPath($request->getSession(), "main", $request->getUri());
+            return $this->redirectToRoute('app_login');//, array('_target_path' => 'ticketing'));
         }
         $days = [[
                 'name' => "Dimanche",
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,31 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
+
+use App\Entity\User;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $factory = new PasswordHasherFactory([
+            'common' => ['algorithm' => 'bcrypt'],
+            'memory-hard' => ['algorithm' => 'sodium'],
+        ]);
+        $passwordHasher = $factory->getPasswordHasher('common');
+
+        $u = new User();
+        $u->setEmail("cpoa-admin@yopmail.com");
+        $u->setRoles(["admin"]);
+        
+        $u->setPassword(
+            $passwordHasher->hash("azertyuiop")
+        );
+        $u->setFirstName("AdminF");
+        $u->setLastName("AdminL");
+
+        $manager->persist($u);
 
         $manager->flush();
     }
