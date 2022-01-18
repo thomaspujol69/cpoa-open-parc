f3b67022e55eb4915cba8d447d8f1b743b25d61b
Gomgom <gommynk@gmaiil.com>
2022-01-18 17:21:31 +0100

    code unique

--- src/Entity/PromoCode.php
+++ src/Entity/PromoCode.php
@@ -25,7 +25,7 @@ class PromoCode
     private $label;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $code;
 
